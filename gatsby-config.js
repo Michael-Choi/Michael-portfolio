c971f9e3116788aecd24fd533e172f34ac4cb0fe
Michael-Choi <Michael.choi@queensu.ca>
2019-10-02 17:04:53 -0400

    fresh start on portfolio website

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,13 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Michael Choi Portfolio`,
+    description: `Michael Choi - Front end developer. Loves to learn new technoloy and work with cool people`,
+    author: `Michael Choi`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
+
     {
       resolve: `gatsby-source-filesystem`,
       options: {
--- src/components/layout.js
+++ src/components/layout.js
@@ -25,7 +25,7 @@ const Layout = ({ children }) => {
 
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
+      <Header siteTitle={""} />
       <div
         style={{
           margin: `0 auto`,
@@ -35,10 +35,11 @@ const Layout = ({ children }) => {
         }}
       >
         <main>{children}</main>
+
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <p>Gatsby</p>
         </footer>
       </div>
     </>
